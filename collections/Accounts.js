95ac683530119fcc4e23564cb08cac72b5773b62
Brian Donghee Shin <ds2791@columbia.edu>
2014-07-29 08:54:08 -0400

    Facebook auth fixes

--- collections/Accounts.js
+++
@@ -1,9 +0,0 @@
-// Not a custom collection, overriding Meteor behavior
-
-Accounts.onCreateUser(function(options, user) {
-  debugger;
-  if (options.profile) {
-    user.profile = options.profile;
-  }
-  return user;
-})
