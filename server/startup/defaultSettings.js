9bd278e82718fd73c3a23d898a04e0b176ad4b3b
Brian Donghee Shin <ds2791@columbia.edu>
2014-07-29 11:30:51 -0400

    Change Heap ID, make settings clearer

--- server/startup/defaultSettings.js
+++ server/startup/defaultSettings.js
@@ -3,9 +3,14 @@ var defaultSettings = {
     host: 'localhost:9200'
   },
   facebook: {
-    appId: '837193842965373',
+    appId: '478856265465801',
     // NOTE: only test app secret. Must be added to developer list.
     secret: '753f5034c3b52c8d689223a6eb026dc9'
+  },
+  public: {
+    // Just here for reference, loaded too late to work
+    // See constants
+    heapID: '252399778'
   }
 };
 
